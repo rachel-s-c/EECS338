73b93a432472beb7c581b9a9cc8d43f2ba30859f
rachelseng <rachelseng@yahoo.com.sg>
2018-10-24 17:13:56 -0500

    applied spacy on articles from newspaper library

--- main338.py
+++ main338.py
@@ -2,34 +2,59 @@
 
 
 import newspaper 
-
-cnn = newspaper.build('http://www.cnn.com')
-
-#for article in bbc.articles:
-#    print(article.url)
-#    print(article.authors)
-#    print(article.keywords)
+import spacy
 
 
-cnn_first = cnn.articles[0]
+cnn = newspaper.build('http://www.cnn.com', memoize_articles = False)
+bbc = newspaper.build('http://www.bbc.com', memoize_articles = False)
+cnn_first = cnn.articles[10]
 cnn_first.download()
 cnn_first.parse()
-cnn_first.nlp()
-print(cnn_first.keywords)
-print(cnn_first.authors)
-print(cnn_first.summary)
+#print(cnn_first.text)
+#cnn_first.nlp()
+#print(cnn_first.keywords)
+#print(cnn_first.authors)
+#print(cnn_first.summary)
+#print(cnn_first.text)
+print(cnn_first.text)
+print(cnn_first.url)
+
+
+
+
+
+
+"""
+from newspaper import fulltext
+import requests
+
+html = requests.get(cnn_first.url).text
+text = fulltext(html)
+print(text)
 
 """
 
+
+
+"""
 from newspaper import Article
-url = 'http://fox13now.com/2013/12/30/new-year-new-laws-obamacare-pot-guns-and-drones/'
+url = 'https://www.cnn.com/2018/10/23/europe/eu-italy-budget-intl/index.html'
 article = Article(url)
 article.download()
 article.parse()
-article.nlp()
-print(article.keywords)
-print(article.authors)
-print(article.summary)
+#article.nlp()
+#print(article.keywords)
+#print(article.authors)
+#print(article.summary)
+print(article.text)
 """
 
-
+nlp = spacy.load('en')
+#doc = nlp('Steve Jobs Apple is looking at buying U.K. startup for $1 billion')
+doc = nlp(cnn_first.text)
+#for token in doc:
+#    print(token.text)
+    
+for ent in doc.ents:
+    print(ent.text, ent.start_char, ent.end_char, ent.label_)
+    
